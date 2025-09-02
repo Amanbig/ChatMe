a7eb80906773fb8d27bba31d3a9e067dd602705e
Amanbig <amanpreetsinghjhiwant@outlook.com>
2025-09-02 23:52:27 +0530

    added theminig

---
+++ src/components/app/title-bar.tsx
@@ -0,0 +1,30 @@
+import { Button } from "../ui/button";
+import { RxCross2 } from "react-icons/rx";
+import { VscChromeMinimize } from "react-icons/vsc";
+import { IoMdSquareOutline } from "react-icons/io";
+import { getCurrentWindow } from '@tauri-apps/api/window';
+import ThemeToggle from "./theme-toggle";
+
+const appWindow = getCurrentWindow();
+
+export default function TitleBar() {
+    return (
+        <div className="flex flex-row items-center justify-between pl-2 drag">
+            <div>
+                <p>ChatMe</p>
+            </div>
+            <div className="no-drag">
+                <ThemeToggle />
+                <Button variant="ghost" onClick={() => appWindow.minimize()}><VscChromeMinimize /></Button>
+                <Button variant="ghost" onClick={async () => {
+                    if (await appWindow.isMaximized()) {
+                        await appWindow.unmaximize();
+                    } else {
+                        await appWindow.maximize();
+                    }
+                }}><IoMdSquareOutline /></Button>
+                <Button variant="ghost" onClick={() => appWindow.close()}><RxCross2 /></Button>
+            </div>
+        </div>
+    );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import TitleBar from "./components/app/title-bar";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <div className="flex flex-col h-screen overflow-hidden">
+      <TitleBar />
+      <div className="flex-1 overflow-hidden">
+        <App />
+      </div>
+    </div>
+  </React.StrictMode>
 );
